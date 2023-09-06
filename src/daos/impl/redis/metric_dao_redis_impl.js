f9fabbdbdd529ad57fccb3c1fdc74cfaa35320de
Mikhail Rashkovskiy <miklyx@gmail.com>
2023-09-06 17:28:05 +0200

    feat: challenge-2

--- src/daos/impl/redis/metric_dao_redis_impl.js
+++ src/daos/impl/redis/metric_dao_redis_impl.js
@@ -59,6 +59,8 @@ const insertMetric = async (siteId, metricValue, metricName, timestamp) => {
   const minuteOfDay = timeUtils.getMinuteOfDay(timestamp);
 
   // START Challenge #2
+
+  await client.zaddAsync(metricKey, minuteOfDay, formatMeasurementMinute(metricValue, minuteOfDay));
   // END Challenge #2
 };
 /* eslint-enable */
